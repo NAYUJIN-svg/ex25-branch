8dba05f2a36c15143abad3da05b893562f8ba691
NAYUJIN <won10ve8953@gmail.com>
2025-10-28 16:29:50 +0900

    add_dbconn
    add_dbconn

---
+++ src/main/java/com/nyg/mybatis/mapper/StudentMapper.java
@@ -0,0 +1,29 @@
+package com.nyg.mybatis.mapper;
+
+import java.util.List;
+
+
+
+import org.apache.ibatis.annotations.Mapper;
+
+import com.nyg.mybatis.domain.Student;
+
+@Mapper
+public interface StudentMapper {
+	
+	List<Student> findAll();
+// 	@Select("SELECT * FROM student WHERE id = #{id}")
+//	Student findById(Long id);
+	
+	Student findById(Long id);
+	
+	
+	void insert(Student student);
+	
+	
+	void update(Student student);
+	
+	
+	void delete(Long id);
+
+}
