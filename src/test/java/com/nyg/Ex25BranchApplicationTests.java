397d23c0023753d24124183583ad90b429e7d87f
NAYUJIN-svg <won10ve8953@gmail.com>
2025-10-29 17:31:33 +0900

    add_tdd
    student테스트 추가

--- src/test/java/com/nyg/Ex25BranchApplicationTests.java
+++ src/test/java/com/nyg/Ex25BranchApplicationTests.java
@@ -1,13 +1,12 @@
 package com.nyg;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class Ex25BranchApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+//	@Test
+//	void contextLoads() {
+//	}
 
 }
